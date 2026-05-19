b02eb7e1c0a334a582cb6c7b93c9ca2bf2b1028c
yuri cassu <64930458+yuricassu@users.noreply.github.com>
2026-05-19 02:49:44 -0300

    ajuste no dbt_buid

--- dags/dbt_build_pipeline.py
+++ dags/dbt_build_pipeline.py
@@ -1,5 +1,9 @@
+from cosmos import DbtTaskGroup, ProjectConfig, ProfileConfig, ExecutionConfig
+from cosmos.profiles import DatabricksTokenProfileMapping
+
 from airflow import DAG
-from airflow.operators.bash import BashOperator
+from airflow.operators.empty import EmptyOperator
+
 from datetime import datetime
 
 with DAG(
@@ -7,23 +11,36 @@ with DAG(
     start_date=datetime(2026, 1, 1),
     schedule="@daily",
     catchup=False,
-    tags=["dbt", "analytics"]
+    tags=["dbt", "databricks", "analytics"]
 ) as dag:
 
-    dbt_build = BashOperator(
-        task_id="dbt_build",
-        bash_command="""
-        cd /usr/local/airflow/dbt &&
-        dbt build
-        """
+    start = EmptyOperator(task_id="start")
+
+    dbt_run = DbtTaskGroup(
+        group_id="dbt_build",
+
+        project_config=ProjectConfig(
+            "/usr/local/airflow/include/dbt"
+        ),
+
+        profile_config=ProfileConfig(
+            profile_name="analytics_copilot",
+            target_name="dev",
+
+            profile_mapping=DatabricksTokenProfileMapping(
+                conn_id="databricks_default",
+                profile_args={
+                    "schema": "silver",
+                    "catalog": "analytics_copilot",
+                }
+            ),
+        ),
+
+        execution_config=ExecutionConfig(
+            dbt_executable_path="/usr/local/bin/dbt",
+        ),
     )
 
-    dbt_docs = BashOperator(
-        task_id="dbt_docs",
-        bash_command="""
-        cd /usr/local/airflow/dbt &&
-        dbt docs generate
-        """
-    )
+    end = EmptyOperator(task_id="end")
 
-    dbt_build >> dbt_docs
+    start >> dbt_run >> end
